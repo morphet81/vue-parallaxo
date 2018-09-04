b23d61494035fecfa95bbf7372eaef622abf0352
alexandre <aobellianne@phoceis.asia>
2018-09-04 18:33:23 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h1>Parallax example</h1> -->
+
+    <parallaxo-background class="parallaxo" src="images/parallax.jpg">
+
+      <h2>This is</h2>
+
+      <h2>An example</h2>
+
+      <h2>Of a</h2>
+
+      <h2>Parallax</h2>
+
+      <h2>Parallax</h2>
+
+    </parallaxo-background>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './components'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.parallaxo {
+  flex-grow: 1;
+
+  h2 {
+    color: black;
+    margin: 20vh 0 0vh 0;
+    padding: 12px;
+    background-color: rgba(165, 165, 165, 0.377);
+  }
 }
 </style>
